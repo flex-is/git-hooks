94aa50ccc81c19c324a475a3dfb2e379cc457f14
Martin Miškovič <miskovic.martin@gmail.com>
2023-03-22 09:41:08 +0100

    Added common message format

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## Unreleased
+### Added
+-   Common message output format
---
+++ src/msg.sh
@@ -0,0 +1,9 @@
+# Common info message output
+function msg_info () {
+    echo "~ [INFO] $1"
+}
+
+# Common error message output
+function msg_error () {
+    echo "~ [ERROR] $1"
+}
